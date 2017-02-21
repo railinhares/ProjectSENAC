81e7b9922098e705930eb90cd8716bf8aa2c4179
raimundo.nlaraujo <raimundo.nlaraujo@CAS511762W0719.senacsp.edu.br>
2017-02-21 20:35:24 -0300

    Versão inicial

---
+++ TesteGit/src/main/java/br/senac/tads/pi3/testegit/Pricipal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.testegit;
+
+/**
+ *
+ * @author raimundo.nlaraujo
+ */
+public class Pricipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
